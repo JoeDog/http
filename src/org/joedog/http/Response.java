16fc5a73f59f13a90ef6cdf0af2e45ddd2d60120
Jeff Fulmer <jeff@joedog.org>
2019-01-07 12:09:16 -0500

    Handling WWW-Authenticate

--- src/org/joedog/http/Response.java
+++ src/org/joedog/http/Response.java
@@ -1,13 +1,18 @@
 package org.joedog.http;
 
-public class Response <K, V> extends Headers {
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
 
+public class Response <K, V> extends Headers {
+  public static final String WWW_AUTHENTICATE  = "WWW-Authenticate";  
   public static final String TRANSFER_ENCODING = "Transfer-Encoding";
 
-  protected int    code       = 0;
-  protected double version    = 1.0;
-  protected String message    = null; 
-  protected StringBuilder res = null;
+  protected int           code      = 0;
+  protected Auth.TYPE     type      = Auth.TYPE.BASIC;
+  protected String        realm     = null;
+  protected double        version   = 1.0;
+  protected String        message   = null; 
+  protected StringBuilder res       = null;
 
   public Response() { 
     this.res = new StringBuilder();
@@ -18,7 +23,18 @@ public class Response <K, V> extends Headers {
     this.res.append("\015\012");
     if (line.startsWith("HTTP/")) {
       this.parseResponse(line);
-    }  
+    }
+    if (line.startsWith(WWW_AUTHENTICATE)) {
+      this.parseAuthenticate(line);
+    }
+  }
+
+  public Auth.TYPE getAuthorizationType() {
+    return this.type;
+  }
+
+  public String getAuthorizationRealm() {
+    return this.realm;
   }
 
   public String toString() {
@@ -38,5 +54,32 @@ public class Response <K, V> extends Headers {
     this.version = Double.parseDouble(res[0].substring(5, res[0].length()));
     this.code    = Integer.parseInt(res[1]);
     this.message = res[2];
+  }
+
+  private void parseAuthenticate(String line) {
+    //WWW-Authenticate: Basic realm="basic area"    
+    String res[] = null;
+    if (! line.contains(":")) {
+      return; // this is an unexpected turn of events....
+    }
+    res = line.split(":", 2);
+    if (res[1] == null || res[1].length() < 1) {
+      return; // WTF??
+    }
+    res[1].trim();
+    if (res[1].toLowerCase().startsWith("basic")) {
+      this.type = Auth.TYPE.BASIC;
+    } else if (res[1].toLowerCase().startsWith("digest")) {
+      this.type = Auth.TYPE.BASIC;
+    } else if (res[1].toLowerCase().startsWith("ntlm")) {
+      this.type = Auth.TYPE.NTLM;
+    } else {
+      this.type = Auth.TYPE.UNSUPPORTED;
+    }
+    Pattern p = Pattern.compile("realm=\"([^\"]*)\"");
+    Matcher m = p.matcher(line);
+    while (m.find()) {
+      this.realm = m.group(1);
+    }    
   }
 }
