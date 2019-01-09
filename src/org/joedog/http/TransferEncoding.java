1690ae31550ea22a423d521372b821b4450a01ed
Jeff Fulmer <jeff@joedog.org>
2019-01-09 14:13:53 -0500

    Implemented FIXED, CHUNKED and NONE

--- src/org/joedog/http/TransferEncoding.java
+++ src/org/joedog/http/TransferEncoding.java
@@ -4,9 +4,19 @@ public class TransferEncoding {
   public  enum TYPE { NONE, FIXED, CHUNKED; }
   private boolean more   = true;
   private int     length = 0;
+  private TYPE    type;
 
-  public TransferEncoding() {
-    
+  public TransferEncoding(Response response) {
+    this.type   = response.getTransferEncoding(); 
+    this.length = response.getContentLength();
+    if (this.length > 0) {
+      // One must be so careful these days...
+      this.type = TYPE.FIXED;
+    }
+  }
+
+  public void noMore() {
+    this.more = false;
   }
 
   public boolean hasMore() {
@@ -14,21 +24,33 @@ public class TransferEncoding {
   }
 
   public int length(Connection conn) {
-    if (this.length > 0) {
-      return this.length;
-    }
-    int    len = 0;
-    String buf = null;
-    while (buf == null) {
-      buf = conn.readline();
-    }
-    try {
-      len = Integer.parseInt(buf, 16);
-    } catch (java.lang.NumberFormatException ignore) {}
+    switch (this.type) {
+      case FIXED: 
+        if (this.length > 0) {
+          this.more = false;
+          return this.length;
+        }
+      case CHUNKED:
+        int    len = 0;
+        String buf = null;
+        while (buf == null) {
+          buf = conn.readline();
+        }
+        try {
+          len = Integer.parseInt(buf, 16);
+        } catch (java.lang.NumberFormatException ignore) {}
 
-    if (len == 0) {
-      this.more = false;
+        if (len == 0) {
+          this.more = false;
+        }
+        return len;
+      case NONE: 
+      default:
+        if (conn.isClosed()) {
+          this.more = false;
+          return 0;
+        }
+        return 1;
     }
-    return len;
   }
 }
