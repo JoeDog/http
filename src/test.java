5f81700a628b2566ae9d0265c767340ac418c467
Jeff Fulmer <jdfulmer@armstrong.com>
2019-03-14 15:23:09 -0400

    Test changes

--- src/test.java
+++ src/test.java
@@ -3,7 +3,7 @@ import org.joedog.http.*;
 public class test {
   public static void main(String [] args) {
     HTTP http = new HTTP();
-    for (int i = 0; i < 5; i++) {
+    for (int i = 0; i < 20; i++) {
       http.get("https://www.joedog.org/siege/cookie-expire.php");
     } 
     //http.get("https://www.joedog.org/siege/cookie-expire.php");
